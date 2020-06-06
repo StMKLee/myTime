84ac7b2d5e73408542c0b83cfd075301555d041b
StMKLee <415501323@qq.com>
2020-06-06 10:57:53 +0800

    修改抽奖页

--- src/pages/cjzp/cjzp.vue
+++ src/pages/cjzp/cjzp.vue
@@ -5,10 +5,20 @@
     <main-cont>
       <div class="allcont">
         <div class="innav">
-          nav
+          <div class="navtxt" @click="showf">盘转</div>
+          <div class="navtxt" @click="shows">针转</div>
+          <div class="navtxt" @click="showt">方块</div>
         </div>
         <div class="zp">
-          1
+          <div v-show="show1" class="rongqi">
+            1
+          </div>
+          <div v-show="show2" class="rongqi">
+            2
+          </div>
+          <div v-show="show3" class="rongqi">
+            3
+          </div>
         </div>
       </div>
     </main-cont>
@@ -23,11 +33,27 @@
   export default{
     data(){
       return{
-
+        show1:true,
+        show2:false,
+        show3:false
       }
     },
     methods:{
-
+      showf:function(){
+        this.show1=true;
+        this.show2=false;
+        this.show3=false;
+      },
+      shows:function(){
+        this.show1=false;
+        this.show2=true;
+        this.show3=false;
+      },
+      showt:function(){
+        this.show1=false;
+        this.show2=false;
+        this.show3=true;
+      }
     },
     components:{
       pageHead,
@@ -71,12 +97,26 @@
   }
   .zp{
     margin-top: 1.25rem;
-    position: relative;
     width: 90%;
     height: auto;
     min-height: 25rem;
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  .innav{
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    position: relative;
+  }
+  .navtxt{
+    font-size: 1.375rem;
+    color: #1D5BDF;
+    opacity: 0.8;
+    text-decoration: underline;
+    cursor: pointer;
   }
 </style>
